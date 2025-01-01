12b6b10b05d03bb1fda3d14d9fd9ff75b513ad20
bumstone <73152527+kobumseouk@users.noreply.github.com>
2025-01-01 15:39:06 +0900

    Title: 배열의 평균, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 250101/배열의 평균/ave-of-array.java
+++ 250101/배열의 평균/ave-of-array.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
         int[][] matrix = new int[2][4];
         double sum;
-        for(int i= 0; i < 2; i++){
+        for(int i = 0; i < 2; i++){
             sum = 0;
             for(int j = 0; j < 4; j++){
                 matrix[i][j] = sc.nextInt();
                 sum += matrix[i][j];
             }
-            System.out.print(sum / 4 + " ");
+            System.out.printf("%.1f ", sum / 4);
         }
         System.out.println();
 
@@ -24,11 +23,10 @@ public class Main {
                 sum += matrix[i][j];
             }
             total += sum;
-            System.out.print(sum / 2 + " ");
+            System.out.printf("%.1f ", sum / 2);
         }
         System.out.println();
 
-        System.out.println(total / 8);
-        
+        System.out.printf("%.1f", total / 8);
     }
 }
