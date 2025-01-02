68faa8146ca49a5a7135d894c4353232c6efdb25
bumstone <73152527+kobumseouk@users.noreply.github.com>
2025-01-03 03:35:27 +0900

    Title: 대각선으로 숫자 채우기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 250103/대각선으로 숫자 채우기/diagonal-numbering.java
+++ 250103/대각선으로 숫자 채우기/diagonal-numbering.java
@@ -12,26 +12,16 @@ public class Main {
         int num = 1;
         //arr[1][1] = num;
 
-        // 모든 시작점을 순회
-        // 첫 행의 모든 열에서 시작
-        for(int startCol = 0; startCol < m; startCol++) {
-            int row = 0;
-            int col = startCol;
-            while(row < n && col >= 0) {
-                arr[row][col] = num++;
-                row++;
-                col--;
-            }
-        }
-        
-        // 첫 열을 제외한 마지막 행의 모든 열에서 시작
-        for(int startRow = 1; startRow < n; startRow++) {
-            int row = startRow;
-            int col = m-1;
-            while(row < n && col >= 0) {
-                arr[row][col] = num++;
-                row++;
-                col--;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(arr[i][j] == 0){
+                    int row = i;
+                    int col = j;
+
+                    while(row < n && col >= 0) {
+                        arr[row++][col--] = num++;
+                    }
+                }
             }
         }
 
