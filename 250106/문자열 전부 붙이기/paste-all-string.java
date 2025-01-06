5d18bbe5afceeda95ff0abf49e09c59b5df881b3
bumstone <73152527+kobumseouk@users.noreply.github.com>
2025-01-06 15:55:22 +0900

    Title: 문자열 전부 붙이기, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250106/문자열 전부 붙이기/paste-all-string.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < n; i++){
+            sb.append(br.readLine());
+        }
+        
+
+        System.out.println(sb.toString());
+    }
+}
