9ab9c316ef160780ec95b53ad5eb96ddc8cc923f
bumstone <73152527+kobumseouk@users.noreply.github.com>
2025-01-09 04:21:56 +0900

    Title: 문자 제거하기, Time: 89ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 250109/문자 제거하기/del-char.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        while(str.length() > 1){
+            int n = Integer.parseInt(br.readLine());
+
+            if(n > str.length()){
+                str = str.substring(0, str.length() - 1);
+            } else {
+                str = str.substring(0, n) + str.substring(n + 1);
+            }
+
+            System.out.println(str);
+        }
+
+
+    }
+}
