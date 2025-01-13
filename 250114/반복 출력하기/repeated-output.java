cef215c6c680b97f09d532c4e6b88fc59bb35476
bumstone <73152527+kobumseouk@users.noreply.github.com>
2025-01-14 00:46:05 +0900

    Title: 반복 출력하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250114/반복 출력하기/repeated-output.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printLines(n);
+
+    }
+
+    public static void printLines(int n) {
+        for(int i = 0; i < n; i++) {
+            System.out.println("12345^&*()_");
+        }
+    }
+}
