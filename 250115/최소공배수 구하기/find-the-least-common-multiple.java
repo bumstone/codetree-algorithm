0b9554b04f8389c24d8fa15a096c919b50d0852e
bumstone <73152527+kobumseouk@users.noreply.github.com>
2025-01-15 01:03:44 +0900

    Title: 최소공배수 구하기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250115/최소공배수 구하기/find-the-least-common-multiple.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.print(LCM(n,m));
+
+    }
+
+    public static int LCM(int n, int m){
+        int result = 1;
+        for(int i = 2; i < Math.min(n, m); i++){
+            if(n % i == 0 && m % i == 0) {
+                result *= i;
+            }
+        }
+
+        return result;
+    }
+}
