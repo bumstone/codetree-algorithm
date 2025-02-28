b76e799f6da6d93aac9c088507a40a99108046bc
bumstone <73152527+kobumseouk@users.noreply.github.com>
2025-03-01 04:32:02 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 241ms, Memory: 27MB, Status: Passed - CodeTree

--- 250301/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 250301/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -16,11 +16,9 @@ public class Main {
         rect_x2[0] = sc.nextInt() + 1000;
         rect_y2[0] = sc.nextInt() + 1000;
 
-        int firstArea = 0;
-        for (int a = rect_x1[0]; a <= rect_x2[0]; a++) {
-            for (int b = rect_y1[0]; b <= rect_y2[0]; b++) {
+        for (int a = rect_x1[0]; a < rect_x2[0]; a++) {
+            for (int b = rect_y1[0]; b < rect_y2[0]; b++) {
                 h[a][b] = 1; 
-                firstArea++; 
             }
         }
 
@@ -30,16 +28,35 @@ public class Main {
         rect_x2[1] = sc.nextInt() + 1000;
         rect_y2[1] = sc.nextInt() + 1000;
 
-        int overlapArea = 0;
-        for (int a = rect_x1[1]; a <= rect_x2[1]; a++) {
-            for (int b = rect_y1[1]; b <= rect_y2[1]; b++) {
-                if (h[a][b] == 1) {
-                    overlapArea++;  
-                }
+        for (int a = rect_x1[1]; a < rect_x2[1]; a++) {
+            for (int b = rect_y1[1]; b < rect_y2[1]; b++) {
+                h[a][b] = 2;
             }
         }
 
-        int result = firstArea - overlapArea;
-        System.out.print(result);
+        // 1, 2 순으로 붙였는데도
+        // 아직 숫자 1로 남아있는 곳들 중 최대 최소 x, y 값을 전부 계산합니다.
+        int minX = h.length, maxX = 0, minY = h.length, maxY = 0;
+        boolean firstRectExist = false;
+        for(int x = 0; x < h.length; x++)
+            for(int y = 0; y < h[x].length; y++)
+                if(h[x][y] == 1) {
+                    firstRectExist = true;
+                    minX = Math.min(minX, x);
+                    maxX = Math.max(maxX, x);
+                    minY = Math.min(minY, y);
+                    maxY = Math.max(maxY, y);
+                }
+        
+        // 넓이를 계산합니다.
+        int area;
+        // Case 1. 첫 번째 직사각형이 전혀 남아있지 않다면 넓이는 0입니다.
+        if(!firstRectExist)
+            area = 0;
+        // Case 2. 첫 번째 직사각형이 남아있다면, 넓이를 계산합니다.
+        else
+            area = (maxX - minX + 1) * (maxY - minY + 1);
+
+        System.out.print(area);
     }
 }
