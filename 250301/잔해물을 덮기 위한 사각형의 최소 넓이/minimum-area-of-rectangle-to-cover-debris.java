f9b28b7727e47d42ffd3017f237eb9c67f3961da
bumstone <73152527+kobumseouk@users.noreply.github.com>
2025-03-01 03:34:31 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 139ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 250301/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 250301/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -12,34 +12,34 @@ public class Main {
 
         // 첫 번째 사각형
         rect_x1[0] = sc.nextInt() + 1000;
-        rect_x2[0] = sc.nextInt() + 1000;
         rect_y1[0] = sc.nextInt() + 1000;
+        rect_x2[0] = sc.nextInt() + 1000;
         rect_y2[0] = sc.nextInt() + 1000;
 
         int firstArea = 0;
-        for (int a = rect_x1[0]; a < rect_x2[0]; a++) {
-            for (int b = rect_y1[0]; b < rect_y2[0]; b++) {
-                h[a][b] = 1; // 첫 번째 사각형 영역 표시
-                firstArea++;  // 첫 번째 사각형의 넓이 증가
+        for (int a = rect_x1[0]; a <= rect_x2[0]; a++) {
+            for (int b = rect_y1[0]; b <= rect_y2[0]; b++) {
+                h[a][b] = 1; 
+                firstArea++; 
             }
         }
 
         // 두 번째 사각형
         rect_x1[1] = sc.nextInt() + 1000;
-        rect_x2[1] = sc.nextInt() + 1000;
         rect_y1[1] = sc.nextInt() + 1000;
+        rect_x2[1] = sc.nextInt() + 1000;
         rect_y2[1] = sc.nextInt() + 1000;
 
         int overlapArea = 0;
-        for (int a = rect_x1[1]; a < rect_x2[1]; a++) {
-            for (int b = rect_y1[1]; b < rect_y2[1]; b++) {
-                if (h[a][b] == 1) { // 이미 첫 번째 사각형이 있는 경우
-                    overlapArea++;  // 겹치는 부분의 넓이 증가
+        for (int a = rect_x1[1]; a <= rect_x2[1]; a++) {
+            for (int b = rect_y1[1]; b <= rect_y2[1]; b++) {
+                if (h[a][b] == 1) {
+                    overlapArea++;  
                 }
             }
         }
 
-        int result = firstArea - overlapArea; // 첫 번째 사각형 넓이에서 겹치는 부분 제외
+        int result = firstArea - overlapArea;
         System.out.print(result);
     }
 }
