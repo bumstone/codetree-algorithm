299c38710af5b27df48ca9e4edcd67b7a3814ab7
bumstone <73152527+kobumseouk@users.noreply.github.com>
2025-03-09 06:46:11 +0900

    Title: 숫자들의 합 중 최대, Time: 304ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250309/숫자들의 합 중 최대/maximum-of-sum-of-numbers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        // Please write your code here.
+        int max = 0;
+        for(int i = x; i <= y; i++) {
+            int digitSum = String.valueOf(i)
+                .chars()
+                .map(Character::getNumericValue)
+                .sum();
+            max = Math.max(max, digitSum);
+        }
+        System.out.print(max);
+
+    }
+}
