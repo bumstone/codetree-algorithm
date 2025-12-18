1fd1d4562ae05452e44781157f1929ec47fd33d0
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2025-12-18 13:22:22 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251218/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+
+        System.out.println(sequence(n));
+    }
+
+    public static int sequence(int n){
+        if(n == 1) return 2;
+        if (n == 2) return 4;
+
+        return (sequence(n - 1) * sequence(n - 2)) % 100;
+
+    }
+}
