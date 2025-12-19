cacc39de23d619ea8b517535c905cc90ffa31bbe
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2025-12-19 23:39:53 +0900

    Title: 단어 정렬, Time: 144ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251219/단어 정렬/sorting-words.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String[] words = new String[n];
+
+        for(int i = 0; i < n; i++){
+            words[i] = sc.next();
+        }
+
+        Arrays.sort(words);
+
+        for(int i = 0; i < n; i++){
+            System.out.println(words[i]);
+        }
+
+    }
+}
