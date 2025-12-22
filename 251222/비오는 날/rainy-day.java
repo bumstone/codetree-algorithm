80ad3b0cedd981e55cc2e5375d901068db5ff38f
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2025-12-22 16:55:40 +0900

    Title: 비오는 날, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

--- 251222/비오는 날/rainy-day.java
+++ 251222/비오는 날/rainy-day.java
@@ -4,21 +4,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int idx = 0;
-        FutureDate[] futureRainyDates = new FutureDate[n];
+        FutureDate earliestRain = null;
+
         for (int i = 0; i < n; i++) {
             String date = sc.next();
             String day = sc.next();
             String weather = sc.next();
             // Please write your code here.
 
-            if(weather.equals("Rain")){
-                futureRainyDates[idx++] = new FutureDate(date, day, weather);
+            if (weather.equals("Rain")) {
+                FutureDate cur = new FutureDate(date, day, weather);
+
+                if (earliestRain == null || cur.date.compareTo(earliestRain.date) < 0) {
+                    earliestRain = cur;
+                }
             }
         }
 
-        
-        if(futureRainyDates.length >= 1) 
-            System.out.println(futureRainyDates[0].date + " " + futureRainyDates[0].day + " " + futureRainyDates[0].weather);
+        if (earliestRain != null) 
+            System.out.println(earliestRain.date + " " + earliestRain.day + " " + earliestRain.weather);
+
     }
 }
 
