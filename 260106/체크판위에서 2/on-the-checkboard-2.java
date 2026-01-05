8be2be4aaf0fbac31348513bdfdcdc030fd58e20
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2026-01-06 02:56:35 +0900

    Title: 체크판위에서 2, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260106/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+        char[][] grid = new char[R][C];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                grid[i][j] = sc.next().charAt(0);
+            }
+        }
+        
+        int count1 = 0, count2 = 0;
+        char curColor = grid[0][0];
+        int ans = 0;
+        // 도약 2번 
+        for(int i = 1; i < R - 2; i++){
+            for(int j = 1; j < C - 2; j++){
+                if(curColor != grid[i][j]){
+                    curColor = grid[i][j];
+                    for(int k = i + 1; k < R - 1; k++){
+                        for(int l = j + 1; l < C - 1; l++){
+                            if(curColor != grid[k][l]){
+                                count2++;
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+
+        System.out.print(count2);
+    }
+}
