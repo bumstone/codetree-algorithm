608a95024494b084cfbcc43109ff16f32cb8ff87
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2026-01-09 13:44:47 +0900

    Title: 개발자의 능력 3, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260109/개발자의 능력 3/ability-of-developer-3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] arr = new int[6];
+    public static int getDiff(int i, int j, int k){
+        int sum1 = arr[i] + arr[j] + arr[k];
+        int sum2 = 0;
+        for(int l = 0; l < 6; l++){
+            sum2 += arr[l];
+        }
+        sum2 -= sum1;
+        return Math.abs(sum2 - sum1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        for (int i = 0; i < 6; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < 6; i++){
+            for(int j = i + 1; j < 6; j++){
+                for(int k = j + 1; k < 6; k++){
+                    min = Math.min(min, getDiff(i, j, k));
+                }
+            }
+        }
+
+        System.out.print(min);
+    }
+}
