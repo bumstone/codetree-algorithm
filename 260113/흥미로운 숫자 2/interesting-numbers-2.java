d5951a455bc129128c371e35778aa0cbff69a83a
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2026-01-13 23:07:15 +0900

    Title: 흥미로운 숫자 2, Time: 164ms, Memory: 19MB, Status: Wrong Answer - Codetree

---
+++ 260113/흥미로운 숫자 2/interesting-numbers-2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean interestNum(int n){
+        int[] counting = new int[10];
+        while(n > 0){
+            counting[n % 10]++;
+            n /= 10;
+        }
+        int cnt = 0;
+        for(int i = 0; i < 10; i++){
+            if(counting[i] > 0) cnt++;
+            if(counting[i] == 1) cnt -= 2;
+        }
+
+        return cnt == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        int cnt = 0;
+        for(int i = x; i <= y; i++){
+            if(interestNum(i)) cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
