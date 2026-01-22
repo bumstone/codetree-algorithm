51be94941fc6c74eeb78f4a773b4d796593c15ee
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2026-01-23 02:03:13 +0900

    Title: 초기 수열 복원하기, Time: 128ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260123/초기 수열 복원하기/restore-initial-sequence.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n - 1];
+        for (int i = 0; i < n - 1; i++)
+            a[i] = sc.nextInt();
+        
+        int[] origin = new int[n];
+        for(int i = 1; i <= n; i++){
+            int idx = 0;
+            int[] counting = new int[n + 1];
+            origin[idx++] = i;
+            counting[i]++;
+            for(int j = 0; j < n - 1; j++){
+                int diff = a[j] - origin[idx - 1];
+                origin[idx++] = diff;
+                counting[diff]++;
+            }
+
+            boolean check = true;
+            for(int j = 1; j <= n; j++){
+                if(counting[j] > 1) {
+                    check = false;
+                    break;
+                }
+            }
+            if(check) break;
+        }
+
+        for(int i = 0; i < n; i++)
+            System.out.print(origin[i] + " ");
+    }
+}
