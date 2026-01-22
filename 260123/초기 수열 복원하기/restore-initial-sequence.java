e5b08aabf22adba18f15a4f1cca4722e2f957e2f
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2026-01-23 02:11:00 +0900

    Title: 초기 수열 복원하기, Time: 228ms, Memory: 15MB, Status: Passed - Codetree

--- 260123/초기 수열 복원하기/restore-initial-sequence.java
+++ 260123/초기 수열 복원하기/restore-initial-sequence.java
@@ -13,13 +13,18 @@ public class Main {
             int[] counting = new int[n + 1];
             origin[idx++] = i;
             counting[i]++;
+
+            boolean check = true;
             for(int j = 0; j < n - 1; j++){
                 int diff = a[j] - origin[idx - 1];
+                if (diff < 0 || diff > n) {
+                    check = false;
+                    break;
+                }
                 origin[idx++] = diff;
                 counting[diff]++;
             }
 
-            boolean check = true;
             for(int j = 1; j <= n; j++){
                 if(counting[j] > 1) {
                     check = false;
