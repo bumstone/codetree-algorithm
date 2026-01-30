394f521b4d24ccdd5c5e1bf799806c9c348da204
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2026-01-30 19:00:38 +0900

    Title: 구간 잘 나누기, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

--- 260130/구간 잘 나누기/divide-sections-well.java
+++ 260130/구간 잘 나누기/divide-sections-well.java
@@ -1,48 +1,81 @@
 import java.util.Scanner;
 public class Main {
-    public static int n, m;
+    
     public static final int MAX_N = 100;
+
+    public static int n, m;
     public static int[] nums = new int[MAX_N];
 
-    public static boolean canDivide(int limit){
-        int count = 1; // 구간의 개수
-        int sum = 0;
+    // public static boolean canDivide(int limit){
+    //     int count = 1; // 구간의 개수
+    //     int sum = 0;
         
-        for (int i = 0; i < n; i++) {
-            if (sum + nums[i] > limit) {
-                count++;       // 새로운 구간 시작
-                sum = nums[i]; // 현재 값을 새 구간의 시작으로
-            } else {
-                sum += nums[i];
-            }
-        }
+    //     for (int i = 0; i < n; i++) {
+    //         if (sum + nums[i] > limit) {
+    //             count++;       // 새로운 구간 시작
+    //             sum = nums[i]; // 현재 값을 새 구간의 시작으로
+    //         } else {
+    //             sum += nums[i];
+    //         }
+    //     }
         
-        return count <= m;
-    }
+    //     return count <= m;
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     n = sc.nextInt();
+    //     m = sc.nextInt();
+    //     int low = 0, high = 0;
+    //     for (int i = 0; i < n; i++){
+    //         nums[i] = sc.nextInt();
+
+    //         low = Math.m(low, nums[i]);
+    //         high += nums[i];
+    //     }
+            
+    //     int min = high;
+    //     while(low <= high){
+    //         int mid = (low + high) / 2;
+
+    //         if(canDivide(mid)){
+    //             min=mid;
+    //             high = mid -1;
+    //         }
+    //         else low = mid + 1;
+    //     }
+
+    //     System.out.print(min);
+    // }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
+        
         int low = 0, high = 0;
-        for (int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){
             nums[i] = sc.nextInt();
-
             low = Math.max(low, nums[i]);
             high += nums[i];
         }
-            
-        int min = high;
-        while(low <= high){
-            int mid = (low + high) / 2;
 
-            if(canDivide(mid)){
-                min=mid;
-                high = mid -1;
+        int ans = high;
+        for(int i = low; i <= high; i++){
+            int cnt = 0;
+            int section = 1;
+            for(int j = 0; j < n; j++){
+                if(nums[j] + cnt > i){
+                    cnt = 0;
+                    section++;
+                }
+
+                cnt += nums[j];
             }
-            else low = mid + 1;
+
+            if(section <= m) ans = Math.min(ans, i);
         }
 
-        System.out.print(min);
+        System.out.print(ans);      
     }
 }
