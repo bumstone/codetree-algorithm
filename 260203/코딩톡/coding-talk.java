5160fe6d7de706204b348d410a1d12f935b26297
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2026-02-03 17:25:26 +0900

    Title: 코딩톡, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260203/코딩톡/coding-talk.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int p = sc.nextInt();
+        char[] c = new char[m];
+        int[] u = new int[m];
+        for (int i = 0; i < m; i++) {
+            c[i] = sc.next().charAt(0);
+            u[i] = sc.nextInt();
+        }
+        
+        boolean[] Read = new boolean[n];
+        if(u[p] == 0) return;
+        for(int i = p - 1; i < m; i++){
+            Read[(int)c[i] - 'A'] = true;
+        }
+
+
+        for(int i = 0; i < n; i++){
+            if (!Read[i]) {
+                char notReadPerson = (char) ('A' + i); 
+                System.out.print(notReadPerson + " ");
+            }
+        }
+    }
+}
