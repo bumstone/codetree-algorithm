b66e552bef2bad68d8ab59a65f7a8c15fcc2d2ec
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2026-02-04 16:22:27 +0900

    Title: 연속된 숫자 만들기 2, Time: 3022ms, Memory: 10MB, Status: Time Limit Exceed - Codetree

--- 260204/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
+++ 260204/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -1,26 +1,35 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        int[] arr = new int[3];
+        for(int i = 0; i < 3; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int a = arr[0];
+        int b = arr[1];
+        int c = arr[2];
         
         int minMovement = 0;
         while(true){
             if(a + 1 == b && a + 2 == c) break;
-            if((Math.abs(b - a) > Math.abs(c - b) && Math.abs(c - b) != 1) || Math.abs(b - a) == 1){
+            if(a + 2 == b || b + 2 == c){
+                minMovement++;
+                break;
+            }
+            if(Math.abs(b - a) > Math.abs(c - b)){
                 a = b;
-                b = (b + c + 1) / 2;
-            } else if((Math.abs(b - a) <= Math.abs(c - b) && Math.abs(b - a) != 1) || Math.abs(c - b) == 1){
+                b = b + 1;
+            } else if(Math.abs(b - a) <= Math.abs(c - b)){
                 c = b;
-                b = (a + b + 1) / 2;
+                b = b - 1;
             }
 
             minMovement++;
         }
-
-
         System.out.print(minMovement);
     }
 }
