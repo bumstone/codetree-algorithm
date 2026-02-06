5f4bda81bb1f8c7f873897982ea6368acd96986c
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2026-02-07 02:26:18 +0900

    Title: L, R 그리고 B, Time: 121ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260207/L, R 그리고 B/l-r-and-b.java
+++ 260207/L, R 그리고 B/l-r-and-b.java
@@ -6,6 +6,7 @@ public class Main {
 
         int xL = 0, yL = 0;
         int xB = 0, yB = 0;
+        int xR = 0, yR = 0;
         for (int i = 0; i < 10; i++) {
             board[i] = sc.next();
             if(board[i].contains("B")){
@@ -14,9 +15,19 @@ public class Main {
             } else if(board[i].contains("L")){
                 xL = i;
                 yL = board[i].indexOf('L');
+            } else if(board[i].contains("R")){
+                xR = i;
+                yR = board[i].indexOf('R');
             }
         }
-        int minDist = Math.abs(xL - xB) + Math.abs(yL - yB);
+        int minDist = Math.abs(xL - xB) + Math.abs(yL - yB) - 1;
+
+        if (xL == xB && xL == xR && Math.min(yL, yB) < yR && yR < Math.max(yL, yB)){
+            minDist += 2;
+        } else if(yL == yB && yL == yR && Math.min(xL, xB) < xR && xR < Math.max(xL, xB)){
+            minDist += 2;
+        }
+
 
         System.out.print(minDist);
     }
