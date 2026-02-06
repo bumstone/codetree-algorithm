7ffe2ef108e76d08eac12834ad7454cca959c85d
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2026-02-06 21:11:43 +0900

    Title: 순위 경쟁2, Time: 139ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260206/순위 경쟁2/ranking-competition2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        int A = 0, B = 0;
+        int winner = 0;
+        for (int i = 0; i < n; i++) {
+            char c = sc.next().charAt(0);
+            int s = sc.nextInt();
+
+            if(c == 'A'){
+                A += s;
+                if(s > 0 && winner < A){
+                    cnt++;
+                    winner = A;
+                } else if(s < 0 && winner - s == A && A < B) cnt++;
+            } else {
+                B += s;
+                if(s > 0 && winner < B){
+                    cnt++;
+                    winner = B;
+                } else if(s < 0 && winner - s == B && B < A) cnt++;
+            }
+
+        }
+
+        System.out.print(cnt);
+    }
+}
