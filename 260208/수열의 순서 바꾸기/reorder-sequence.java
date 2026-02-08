0440a36dcc64d4044897a8dc55c323772a15e809
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2026-02-08 15:18:21 +0900

    Title: 수열의 순서 바꾸기, Time: 137ms, Memory: 10MB, Status: Passed - Codetree

--- 260208/수열의 순서 바꾸기/reorder-sequence.java
+++ 260208/수열의 순서 바꾸기/reorder-sequence.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        char[] arr = new char[n];
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.next().charAt(0);
+            arr[i] = sc.nextInt();
         }
         
         int cnt = 1;
