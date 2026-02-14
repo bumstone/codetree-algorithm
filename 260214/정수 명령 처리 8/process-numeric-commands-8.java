4209cf872a5d3bf8cb8a6c730c096b3efc0feae2
Kobumseouk <73152527+bumstone@users.noreply.github.com>
2026-02-14 19:50:51 +0900

    Title: 정수 명령 처리 8, Time: 208ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 260214/정수 명령 처리 8/process-numeric-commands-8.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int n = sc.nextInt();
+
+        // 연결링크드리스트 선언
+        LinkedList<Integer> l = new LinkedList<>();
+        
+        for(int i = 0; i < n; i++) {
+            String command = sc.next();
+
+            if(command.equals("push_front")) {
+                int x = sc.nextInt();
+                l.addFirst(x);
+            }
+            else if(command.equals("push_back")) {
+                int x = sc.nextInt();
+                l.addLast(x);
+            } 
+            else if(command.equals("pop_front")) {
+                System.out.println(l.pollFirst());
+            }
+            else if(command.equals("pop_back")) {
+                System.out.println(l.pollLast());
+            }
+            else if(command.equals("size")) {
+                System.out.println(l.size());
+            }
+            else if(command.equals("empty")) {
+                if(l.isEmpty())
+                    System.out.println(1);
+                else
+                    System.out.println(0);
+            }
+            else if(command.equals("front")) {
+                System.out.println(l.peekFirst());
+            }
+            else {
+                System.out.println(l.peekLast());
+            }
+        }
+    }
+}
